52348ac92b3962576c5d86fa37bf4fd5876a12df
Giorgi <giorgigeliashvili22@gmail.com>
2024-05-03 00:32:10 +0400

    main

--- functions.py
+++ functions.py
@@ -37,14 +37,11 @@ def get_account_details(iban):
         print("Invalid IBAN")
 
 def calculate_loan(iban, loan_amount):
-    if iban in accounts:
-        LOAN_INTEREST_RATE = 0.082  # Fixed loan interest rate (8.2%)
-        total_payable = loan_amount * (1 + LOAN_INTEREST_RATE)
-        print(f"Total payable amount (including interest): {total_payable} GEL")
-        confirm_loan = input("Do you want to proceed with the loan? (y/n) ")
-        if confirm_loan.lower() == "y":
-            accounts[iban]['balance'] += loan_amount
-            add_transaction(iban, "Loan", loan_amount)
-            print("Loan amount added to your account balance.")
-    else:
-        print("Invalid IBAN")
+    LOAN_INTEREST_RATE = 0.082  # Fixed loan interest rate (8.2%)
+    total_payable = loan_amount * (1 + LOAN_INTEREST_RATE)
+    print(f"Total payable amount (including interest): {total_payable} GEL")
+    confirm_loan = input("Do you want to proceed with the loan? (y/n) ")
+    if confirm_loan.lower() == "y":
+        accounts[iban]['balance'] += loan_amount
+        add_transaction(iban, "Loan", loan_amount)
+        print("Loan amount added to your account balance.")
